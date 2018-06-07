2675e3209ff7ae758bc72b5d7ecf3621d6e54d12
ashok <atandan@reppify.com>
2018-06-07 18:30:20 +0530

    login

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var fs = require('fs');
+var cors = require('cors')
 var bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 var app = express();
 var configdata = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
 global.config = configdata.config;
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,7 +17,13 @@ app.get('/', async function (req, res) {
     let data = await db.collection("users").find().toArray();
     res.json(data)
 })
-
+app.post('/login', async function (req, res) {
+    console.info(req.body)
+    let dbo = await MongoClient.connect(global.config.mongourl);
+    let db = dbo.db();
+    let user = await db.collection("users").findOne({'email':req.body.email,'pass':req.body.pass});
+    res.json(user)
+})
 app.post('/insert', async function (req, res) {
     let dbo = await MongoClient.connect(global.config.mongourl);
     let db = dbo.db();
