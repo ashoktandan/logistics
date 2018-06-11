b7567820967ec58dc05cda66085b21aa41346451
ashok <atandan@reppify.com>
2018-06-11 12:15:14 +0530

    api

--- api/index.js
+++ api/index.js
@@ -14,29 +14,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async function (req, res) {
-let dbo = await MongoClient.connect(global.config.mongourl);
-let db = dbo.db();
-let data = await db.collection("users").find().toArray();
-res.json(data)
+    let dbo = await MongoClient.connect(global.config.mongourl);
+    let db = dbo.db();
+    let data = await db.collection("users").find().toArray();
+    res.json(data)
 })
 app.post('/login', async function (req, res) {
-console.info(req.body)
-let dbo = await MongoClient.connect(global.config.mongourl);
-let db = dbo.db();
-let user = await db.collection("users").findOne({ 'email': req.body.email, 'pass': req.body.pass });
-let token = false;
-if (user) {
-token = jwt.sign({
-data: user.email
-}, global.config.secret, { expiresIn: 60 * 60 });
-}
-res.json(token)
+    console.info(req.body)
+    let dbo = await MongoClient.connect(global.config.mongourl);
+    let db = dbo.db();
+    let user = await db.collection("users").findOne({ 'email': req.body.email, 'pass': req.body.pass });
+    if (user) {
+        delete user.pass;
+        user['token'] = jwt.sign({
+            data: user.email
+        }, global.config.secret, { expiresIn: 60 * 60 });
+    }
+    res.json(user)
 })
+
 app.post('/register', async function (req, res) {
-let dbo = await MongoClient.connect(global.config.mongourl);
-let db = dbo.db();
-let saveddata = await db.collection("users").insertOne(req.body);
-res.json(saveddata)
+    let dbo = await MongoClient.connect(global.config.mongourl);
+    let db = dbo.db();
+    let saveddata = await db.collection("users").insertOne(req.body);
+    res.json(saveddata)
 })
 
 app.listen(global.config.port)
