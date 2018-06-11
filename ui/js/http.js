2636d316065aa522cc81137309cc2f701b65078e
ashok <atandan@reppify.com>
2018-06-11 10:43:16 +0530

    test

--- ui/js/http.js
+++ ui/js/http.js
@@ -1,13 +1,13 @@
 async function postcall(api, data) {
-    console.info(`Hitting API ="${localStorage.api + api}" with data ="${JSON.stringify(data)}"`)
+    console.info(`Hitting API ="${localStorage.api + api}" with data ="${data}"`)
     let response = await fetch(localStorage.api + api, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-    'content-type': 'application/json'
-    }
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'content-type': 'application/json'
+        }
     })
     let responsedata = await response.json()
     console.info(`Got response="${responsedata}" from API =${localStorage.api + api}`)
     return responsedata
-    }
+}
