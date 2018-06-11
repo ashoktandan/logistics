03670c13388b0efa6cd134ef360a4d10638c5b1f
vijaysingh <vijayk@reppify.com>
2018-06-11 10:35:31 +0530

    final ui

--- api/index.js
+++ api/index.js
@@ -14,29 +14,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async function (req, res) {
-    let dbo = await MongoClient.connect(global.config.mongourl);
-    let db = dbo.db();
-    let data = await db.collection("users").find().toArray();
-    res.json(data)
+let dbo = await MongoClient.connect(global.config.mongourl);
+let db = dbo.db();
+let data = await db.collection("users").find().toArray();
+res.json(data)
 })
 app.post('/login', async function (req, res) {
-    console.info(req.body)
-    let dbo = await MongoClient.connect(global.config.mongourl);
-    let db = dbo.db();
-    let user = await db.collection("users").findOne({ 'email': req.body.email, 'pass': req.body.pass });
-    let token = false;
-    if (user) {
-        token = jwt.sign({
-            data: user.email
-        }, global.config.secret, { expiresIn: 60 * 60 });
-    }
-    res.json(token)
+console.info(req.body)
+let dbo = await MongoClient.connect(global.config.mongourl);
+let db = dbo.db();
+let user = await db.collection("users").findOne({ 'email': req.body.email, 'pass': req.body.pass });
+let token = false;
+if (user) {
+token = jwt.sign({
+data: user.email
+}, global.config.secret, { expiresIn: 60 * 60 });
+}
+res.json(token)
 })
-app.post('/insert', async function (req, res) {
-    let dbo = await MongoClient.connect(global.config.mongourl);
-    let db = dbo.db();
-    let saveddata = await db.collection("users").insertOne(req.body);
-    res.json(saveddata)
+app.post('/register', async function (req, res) {
+let dbo = await MongoClient.connect(global.config.mongourl);
+let db = dbo.db();
+let saveddata = await db.collection("users").insertOne(req.body);
+res.json(saveddata)
 })
 
 app.listen(global.config.port)
---
+++ ui/js/http.js
@@ -0,0 +1,13 @@
+async function postcall(api, data) {
+    console.info(`Hitting API ="${localStorage.api + api}" with data ="${JSON.stringify(data)}"`)
+    let response = await fetch(localStorage.api + api, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+    'content-type': 'application/json'
+    }
+    })
+    let responsedata = await response.json()
+    console.info(`Got response="${responsedata}" from API =${localStorage.api + api}`)
+    return responsedata
+    }
